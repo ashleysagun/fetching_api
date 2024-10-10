279b2cb6afa22cb9e3f0d12e23f8fb2a085f44dd
ashleysagun <sagunashley48@gmail.com>
2024-10-10 12:40:38 +0800

    master

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import {
   QueryClientProvider,
   useQuery,
 } from '@tanstack/react-query'
-
 import {
   Table,
   TableBody,
@@ -28,44 +27,47 @@ function Example() {
   const { isLoading, error, data } = useQuery({
     queryKey: ['repoData'],
     queryFn: () =>
-      fetch('https://api.github.com/repos/TanStack/query').then((res) =>
+      fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population').then((res) =>
         res.json(),
+    
+    
+  
       ),
+      
   })
 
   if (isLoading) return 'Loading...'
 
   if (error) return 'An error has occurred: ' + error.message
 
+  const user_data=data.data
+  
+
   return (
-    <div>
-      <h1>{data.name}</h1>
-      <p>{data.description}</p>
-      <strong>👀 {data.subscribers_count}</strong>{' '}
-      <strong>✨ {data.stargazers_count}</strong>{' '}
-      <strong>🍴 {data.forks_count}</strong>
-    </div>
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">ID Nation</TableHead>
+          <TableHead className="text-right">Nation</TableHead>
+          <TableHead className="text-right">ID Year</TableHead>
+          <TableHead className="text-right">Year</TableHead>
+          <TableHead className="text-right">Population</TableHead>
+          <TableHead className="text-right">Slug Nation</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {user_data.map((data, key) => (
+          <TableRow key={key}>
+            <TableCell className="font-medium">{data['ID Nation']}</TableCell>
+            <TableCell className="text-right">{data.Nation}</TableCell>
+            <TableCell className="text-right">{data['ID Year']}</TableCell>
+            <TableCell className="text-right">{data.Year}</TableCell>
+            <TableCell className="text-right">{data.Population}</TableCell>
+            <TableCell className="text-right">{data['Slug Nation']}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+     
+      </Table>
   )
 }
-
-
-<Table>
-  <TableCaption>A list of your recent invoices.</TableCaption>
-  <TableHeader>
-    <TableRow>
-      <TableHead className="w-[100px]">Nation</TableHead>
-      <TableHead>Status</TableHead>
-      <TableHead>Method</TableHead>
-      <TableHead className="text-right">Year</TableHead>
-    </TableRow>
-  </TableHeader>
-  <TableBody>
-    <TableRow>
-      <TableCell className="font-medium"></TableCell>
-      <TableCell>Paid</TableCell>
-      <TableCell>Credit Card</TableCell>
-      <TableCell className="text-right">$250.00</TableCell>
-    </TableRow>
-  </TableBody>
-</Table>
-(user.data.)
